526ab135243a01446a5710d9fc6982d366c417d9
Fikret Musk <54106274+fikret0@users.noreply.github.com>
2021-03-03 19:27:57 +0300

    Update copyright.js

--- assets/js/copyright.js
+++ assets/js/copyright.js
@@ -1,4 +1,5 @@
 window.onload = function()
 {
     console.log("Bu site fikret tarafından yapılmıştır. GitHub: @fikret0");
+    //bu dosyayı silmek veya editlemek sana telif hakkı davası açmamı sağlar.
 }
